f2004a0ec8b0792259d24ff7a8a7273ef2c9abe9
commanderturner <commanderturner@gmail.com>
2015-01-11 12:20:09 +0000

    initial

---
+++ public/javascripts/piratemap.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    var myOSM;
+    myOSM = L.tileLayer('http://ajax.pirates.42monkeys.co.uk/osm_tiles/{z}/{x}/{y}.png', 
+	{
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    });
+    var baseMaps = { "myOSM": myOSM };
+    var southWest = L.latLng(50, -4), northEast = L.latLng(55, 4), bounds = L.latLngBounds(southWest, northEast);
+    var map = L.map('map', 
+    {
+        center: [52.0565, -2.7160],
+        zoom: 17,
+        layers: [myOSM],
+    });
+    L.control.layers(baseMaps).addTo(map);
+})();
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+﻿
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    res.render('index', { title: 'Express', year: new Date().getFullYear() });
+};
+
+exports.about = function (req, res) {
+    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page.' });
+};
+
+exports.contact = function (req, res) {
+    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page.' });
+};
+
+exports.piratemap = function (req, res) {
+    res.render('piratemap', { title: 'Pirate map', year: new Date().getFullYear(), message: 'The map of the pirate world.' });
+};
