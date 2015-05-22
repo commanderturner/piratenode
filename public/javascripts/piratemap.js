b3294ff58e355a2bf1093c34060ee563c6d491a9
Mark Turner <commanderturner@gmail.com>
2015-05-22 20:39:46 +0100

    added ports to map and the backdrop of natural earth raster, ports need changing to circles and styling 	deleted:    public/javascripts/leaflet-src.js 	deleted:    public/javascripts/leaflet.js 	modified:   public/javascripts/piratemap.js 	deleted:    public/stylesheets/images/layers-2x.png 	deleted:    public/stylesheets/images/layers.png 	deleted:    public/stylesheets/images/marker-icon-2x.png 	deleted:    public/stylesheets/images/marker-icon.png 	deleted:    public/stylesheets/images/marker-shadow.png 	modified:   views/layout.jade 	modified:   views/piratemap.jade
    	public/javascripts/ports.js
    	public/plugins/

--- public/javascripts/piratemap.js
+++ public/javascripts/piratemap.js
@@ -1,16 +1,157 @@
 ﻿(function () {
+ var ports = { "type": "FeatureCollection",
+              "features": [
+                { "type": "Feature",
+                  "geometry": {
+                    "type": "Point", "coordinates": [-77.345,25.06]
+                  },
+                  "properties": {
+                    "name": "Nassau",
+                    "nation": "English"
+                  }
+                },
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-82.331543,23.059516]},
+                  "properties": {
+                    "name": "Havana",
+                    "nation": "Spanish"
+                  }
+                },        
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-72.79,20.039722]},
+                  "properties": {
+                    "name": "Tortuga",
+                    "nation": "French"
+                  }
+                },
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-69.890784,18.479014]},
+                  "properties": {
+                    "name": "Santo Domingo",
+                    "nation": "Spanish"
+                  }
+                },
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-72.8669444,18.4313889]},
+                  "properties": {
+                    "name": "Petite Goave",
+                    "nation": "French"
+                  }
+                },
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-76.841, 17.937]},
+                  "properties": {
+                    "name": "Port Royal",
+                    "nation": "English"
+                  }
+                },
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-81.374722, 13.348889]},
+                  "properties": {
+                    "name": "Old Providence",
+                    "nation": "English"
+                  }
+                },
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-75.5,10.4]},
+                  "properties": {
+                    "name": "Cartagena",
+                    "nation": "Spanish"
+                  }
+                },
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-68.928051,12.123586]},
+                  "properties": {
+                    "name": "Curacao",
+                    "nation": "Dutch"
+                  }
+                }, 
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-66.9, 10.5]},
+                  "properties": {
+                    "name": "Caracas",
+                    "nation": "Spanish"
+                  }
+                },
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-61.507809, 10.657394]},
+                  "properties": {
+                    "name": "Trinidad",
+                    "nation": "Spanish"
+                  }
+                }, 
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-59.625183, 13.105909]},
+                  "properties": {
+                    "name": "Bridgetown",
+                    "nation": "English"
+                  }
+                }, 
+                { "type": "Feature",
+                  "geometry": {"type": "Point", "coordinates": [-61.7138203, 16.0101938]},
+                  "properties": {
+                    "name": "Basse-Terre",
+                    "nation": "French"
+                  }
+                }, 
+                { "type": "Feature",
+                  "geometry": {
+                    "type": "Point", "coordinates": [-63.0569535, 18.0296587]
+                  },
+                  "properties": {
+                    "name": "St. Maarten",
+                    "nation": "Dutch"
+                  }
+                }, 
+                { "type": "Feature",
+                  "geometry": {
+                    "type": "Point", "coordinates": [-66.0583415, 18.3848264]
+                  },
+                  "properties": {
+                    "name": "San Juan",
+                    "nation": "Spanish"
+                  }
+                },
+                { "type": "Feature",
+                  "geometry": {
+                    "type": "Point", "coordinates": [-61.8443254, 17.1244877]
+                  },
+                  "properties": {
+                    "name": "St. John",
+                    "nation": "English"
+                  }
+                }
+            ]
+          };   
     var myOSM;
     myOSM = L.tileLayer('http://ajax.pirates.42monkeys.co.uk/osm_tiles/{z}/{x}/{y}.png', 
 	{
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     });
-    var baseMaps = { "myOSM": myOSM };
-    var southWest = L.latLng(50, -4), northEast = L.latLng(55, 4), bounds = L.latLngBounds(southWest, northEast);
+    var pirateOceanBackdrop;
+    pirateOceanBackdrop = L.tileLayer('http://ajax.pirates.42monkeys.co.uk/pirate_ocean/{z}/{x}/{y}.png', 
+    {
+        attribution: '&copy; <a href="http://www.naturalearthdata.com">Natural Earth</a>'
+    });    
+    var baseMaps = {"myOSM": myOSM, "Pirate Oceans": pirateOceanBackdrop };
+    var portsGeoJson = L.geoJson(ports, {
+        icon: function(feature) {
+            switch (feature.properties.nation) {
+                case 'English': return {color: "#ff0000"};
+                case 'Spanish':   return {color: "#FFF200"};
+                case 'Dutch': return {color: "#00FFDD"};
+                case 'French':   return {color: "#2A00FF"};            
+            }
+        }
+    })    
     var map = L.map('map', 
     {
-        center: [52.0565, -2.7160],
-        zoom: 17,
-        layers: [myOSM],
+        center: [18.5, -76.5],
+        zoom: 6,
+        layers: [myOSM,  pirateOceanBackdrop, portsGeoJson],
     });
-    L.control.layers(baseMaps).addTo(map);
+    var layerControl = L.control;
+    layerControl.layers(baseMaps).addTo(map);
+    pirateOceanBackdrop.bringToFront();
+
 })();
